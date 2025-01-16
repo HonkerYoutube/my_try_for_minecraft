15c5c75ead25819808d383790e5baf7e296c76ad
hikee <honzikpaulik2@gmail.com>
2025-01-16 22:23:36 +0100

    updated vertices for better readability

--- src/main.cpp
+++ src/main.cpp
@@ -174,10 +174,11 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.5f,  0.5f,  // top right
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.5f, -0.5f,  // bottom right
-       -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,  -0.5f, -0.5f,  // bottom left
-       -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,  -0.5f,  0.5f,  // top left 
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
     }; 
     unsigned int indices[] = {
         0, 1, 3,   // first triangle
