15ddb700ccc67e72fb50f6c23f2f6b0ae0b2c7bc
hikee <honzikpaulik2@gmail.com>
2025-01-16 19:49:36 +0100

    textured and colored rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,10 @@
 
 
 
+
+
+
+
 // GLFW callback to adjust viewport size when window is resized
 static void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -28,6 +32,9 @@ static void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
+
+
+
 
 
 
@@ -167,18 +174,27 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        // Positions         // Colors
-       -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   -0.5f, -0.5f,  // Vertex 1: red
-        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,    0.5f, -0.5f,  // Vertex 2: green
-       -0.5f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   -0.5f,  0.5f  // Vertex 3: blue
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.5f,  0.5f,  // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.5f, -0.5f,  // bottom right
+       -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,  -0.5f, -0.5f,  // bottom left
+       -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,  -0.5f,  0.5f,  // top left 
+    }; 
+    unsigned int indices[] = {
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
     };
-    unsigned int VBO, VAO;
+
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -234,10 +250,12 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+
         // draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0); // no need to unbind it every time 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -249,6 +267,7 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
